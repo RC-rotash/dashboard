320a9cce052ac4cc1ffac5db9f02a7f06898c7f0
Rotash <rotash.verma@reliableCharge.com>
2026-04-11 18:07:24 +0530

    pull

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,3 @@
-// // next.config.js
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'http://ec2-13-203-195-172.ap-south-1.compute.amazonaws.com/v1/analysis/:path*',
-//       },
-//     ];
-//   },
-//   output: 'export',  
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -29,8 +9,6 @@ const nextConfig = {
       },
     ];
   },
-  output: 'standalone',  // ✅ Change 'export' to 'standalone'
-  // images: { unoptimized: true }  // standalone mein iski zaroorat nahi
 };
 
 module.exports = nextConfig;
